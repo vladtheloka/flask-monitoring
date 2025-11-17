e2f27c4002b83acfdc56f03e691e8b55af00ff40
vladtheloka <lokatheloka@gmail.com>
2025-11-17 17:04:31 +0000

    dsf

--- restmon/os_platform.py
+++ restmon/os_platform.py
@@ -1,10 +1,15 @@
 from subprocess import Popen,PIPE
+from flask_restful import Resource
 
 class getPlatform(Resource):
 	def get(self):
-		kernel=Popen(['uname','-r'],stdout=PIPE).communicate()[0].rstrip('\n')
-		hostname=Popen(['uname','-n'],stdout=PIPE).communicate()[0].rstrip('\n')
-		architecture=Popen(['uname','-m'],stdout=PIPE).communicate()[0].rstrip('\n')
-		os=Popen(['uname','-o'],stdout=PIPE).communicate()[0].rstrip('\n')
+		kernel=Popen(['uname','-r'],stdout=PIPE).communicate() \
+		[0].rstrip('\n')
+		hostname=Popen(['uname','-n'],stdout=PIPE).communicate() \
+		[0].rstrip('\n')
+		architecture=Popen(['uname','-m'],stdout=PIPE).communicate() \
+		[0].rstrip('\n')
+		os=Popen(['uname','-o'],stdout=PIPE).communicate() \
+		[0].rstrip('\n')
 		return {'kernelversion' : kernel, 'operatingsystem' : os,'hostname' \
 		: hostname, 'architecture' : architecture}
