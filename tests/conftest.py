616bc25deb888e4ee3dfc089cad2091c71d37442
Vlad <lokatheloka@gmail.com>
2025-11-23 20:33:29 +0100

    ds

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,16 @@
 import pytest
-from flask.testing import FlaskClient
+from restmon.api import create_app
 from flask import Flask
+from flask.testing import FlaskClient
+
+@pytest.fixture
+def app() -> Flask:
+    """Возвращает Flask app для тестов."""
+    app = create_app()
+    app.testing = True
+    return app
 
 @pytest.fixture
 def client(app: Flask) -> FlaskClient:
-    app.testing = True
+    """Возвращает тестовый клиент Flask."""
     return app.test_client()
