71d435b82cc9a20219be9141d84f4d603a846b31
vladtheloka <lokatheloka@gmail.com>
2025-11-23 18:51:51 +0000

    dsa

--- restmon/types.py
+++ restmon/types.py
@@ -2,8 +2,4 @@ from typing import Any, Protocol
 
 
 class SupportsResource(Protocol):
-    """
-    Protocol for Flask-RESTful resources.
-    Helps Pylance understand add_resource().
-    """
     def get(self, *args: Any, **kwargs: Any) -> Any: ...
