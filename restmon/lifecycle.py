d4b4419b1ca4ab88cbfce7ff745e336de8cb2cd4
vladtheloka <lokatheloka@gmail.com>
2025-12-23 08:50:55 +0000

    bugs

--- restmon/lifecycle.py
+++ restmon/lifecycle.py
@@ -4,6 +4,7 @@ from restmon.state import mark_shutting_down
 
 def setup_signal_handlers() -> None:
     signal.signal(signal.SIGTERM, _handle_sigterm) # type: ignore
+    signal.signal(signal.SIGINT, _handle_sigterm) # type: ignore
 
 def _handle_sigterm(signum, frame): # type: ignore
     mark_shutting_down()
--- restmon/signal_handlers.py
+++
@@ -1,9 +0,0 @@
-import signal
-from restmon.state import mark_shutting_down
-
-
-def setup_signal_handlers() -> None:
-    def _handle_sigterm(signum, frame): # type: ignore
-        mark_shutting_down()
-
-    signal.signal(signal.SIGTERM, _handle_sigterm) # type: ignore
