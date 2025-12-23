0ab9f41561afcc65295f2a265698bcf844c3e0f5
vladtheloka <lokatheloka@gmail.com>
2025-12-23 11:07:34 +0000

    gj

--- restmon/health.py
+++ restmon/health.py
@@ -12,12 +12,8 @@ class Ready(Resource):
     def get(self):
         if state.shutdown_event.is_set():
             return {"status": "not_ready"}, 503
-
         try:
-            uptime = SystemResources.get_system_uptime()
-            if isinstance(uptime, (int, float)) and uptime > 0: # type: ignore
-                return {"status": "ready"}, 200
+            SystemResources.get_system_uptime()
         except Exception:
-            pass
-        
-        return {"status": "not_ready"}, 503
+            return {"status": "not_ready"}, 503
+        return {"status": "ready"}, 200
