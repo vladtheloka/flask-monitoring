eefc5c19059c2a578fdb7c6c0b84c76cefe36c3e
vladtheloka <lokatheloka@gmail.com>
2025-12-23 17:58:11 +0000

    43r

--- restmon/__main__.py
+++ restmon/__main__.py
@@ -1,15 +1,5 @@
 import os
-import signal
 from restmon.api import create_app
-from restmon.state import set_not_ready
-
-
-def handle_sigterm(signum, frame): # type: ignore
-    print("SIGTERM received, switching to not_ready")
-    set_not_ready()
-
-
-signal.signal(signal.SIGTERM, handle_sigterm) # type: ignore
 
 if __name__ == "__main__":
     app = create_app()
