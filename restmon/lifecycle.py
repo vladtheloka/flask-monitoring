9fa149168c0b4c6bfad5e7dbad779fc7807c3f31
vladtheloka <lokatheloka@gmail.com>
2025-12-24 09:49:55 +0000

    dsf

--- restmon/lifecycle.py
+++ restmon/lifecycle.py
@@ -6,9 +6,10 @@ import logging
 log = logging.getLogger(__name__)
 
 def setup_signal_handlers() -> None:
-    def _handle_shutdown(signum, frame): # type: ignore
-        log.info("Received signal %s, entering shudown", signum) # type: ignore
-        shutdown_event.set()
 
     signal.signal(signal.SIGTERM, _handle_shutdown) # type: ignore
     signal.signal(signal.SIGINT, _handle_shutdown) # type: ignore
+
+def _handle_shutdown(signum, frame): # type: ignore
+        log.info("Received signal %s, entering shudown", signum) # type: ignore
+        shutdown_event.set()
